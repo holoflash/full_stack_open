9a9215391d9ad4c212f43cdd8c5cf2e18003d4f7
holoflash <mikepalacemusic@gmail.com>
2025-01-19 10:47:47 +0100

    Add linter

--- part_3/phonebook-backend/models/person.js
+++ part_3/phonebook-backend/models/person.js
@@ -3,44 +3,44 @@ require('dotenv').config()
 
 mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 console.log('Connecting to mongo')
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: function (v) {
-                return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number! A valid number should be in the format XX-YYYYYYYY or XXX-YYYYYYYY`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! A valid number should be in the format XX-YYYYYYYY or XXX-YYYYYYYY`
     }
-});
+  }
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
